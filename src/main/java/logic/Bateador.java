32e312ac6fcb47a28aebf87b105cf0b1efbd3ce1
manuel <MEPA0001@CE.PUCMM.EDU.DO>
2025-03-27 16:57:05 -0400

    Arreglo de Bateador.

--- src/main/java/logic/Bateador.java
+++ src/main/java/logic/Bateador.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 public class Bateador extends Jugador {
     private String posicion;
     private EstadisticasBateador stats;
+    private int turnos = 0;
+    private boolean lesionado = false;
 
     public Bateador(String nombre, LocalDate fNacimiento, float altura, ArrayList<Lesion> historialLesiones,
                     String posicion, EstadisticasBateador stats) {
@@ -22,8 +24,38 @@ public class Bateador extends Jugador {
         return stats;
     }
 
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public int getTurnos() {
+        return turnos;
+    }
+
+    public void setTurnos(int turnos) {
+        this.turnos = turnos;
+    }
+
+    public void setStats(EstadisticasBateador stats) {
+        this.stats = stats;
+    }
+
+    public void incrementTurnos() {
+        turnos++;
+    }
+
+    public boolean isLesionado() {
+        return lesionado;
+    }
+
+    public void setLesionado(boolean lesionado) {
+        this.lesionado = lesionado;
+    }
+
     @Override
     public void actualizarEstadisticas(Partido partido) {
 
     }
+
+
 }
