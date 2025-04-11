cbd17f0d0b53889892289910fc0428ac40092ebd
manuel <MEPA0001@CE.PUCMM.EDU.DO>
2025-04-10 20:12:26 -0400

    ...

--- src/main/java/logic/Server.java
+++ src/main/java/logic/Server.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ServerJSON {
-    public static void startServer() {
+public class Server implements Runnable {
+
+    @Override
+    public void run() {
         try (ServerSocket serverSocket = new ServerSocket(5000)) {
             System.out.println("Servidor: Esperando conexiones...");
 
